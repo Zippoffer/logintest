2708d1c2b678ab1ce6a5a2a2798d03f086ca4f63
Christopher Hill <thorsdaddy77@gmail.com>
2016-09-27 16:37:31 -0500

    connected to mlabs

--- database.js
+++ database.js
@@ -2,7 +2,8 @@
 
 const mongoose = require('mongoose')
 
-const MONGODB_URL = 'mongodb://localhost:27017/logintothis'
+// const MONGODB_URL = 'mongodb://localhost:27017/logintothis'
+const MONGODB_URL = 'mongodb://me:mee@ds041486.mlab.com:41486/logintothis'
 
 
 mongoose.Promise = Promise
